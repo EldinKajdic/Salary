d736993ff6fa3a80f72aa871a984ef296282f80b
Eldin Kajdic <eldinkajdic@hotmail.com>
2017-10-24 18:15:18 +0200

    Added some error-control

--- SalarySystem/SalaryUWP/AddUser.xaml.cs
+++ SalarySystem/SalaryUWP/AddUser.xaml.cs
@@ -28,40 +28,59 @@ namespace SalaryUWP
 
         private async void AddUserClick(object sender, RoutedEventArgs e)
         {
+
             string name = userNameTb.Text;
             string email = userEmailTb.Text;
             string password = userPasswordTb.Password.ToString();
 
             if (name.Equals("") || email.Equals("") || password.Equals(""))
             {
-                createStatusText.Text = $"The account could not be created.";
-                createStatusTextTwo.Text = $"Please enter information in all fields.";
+                createStatusText.Text = "The account could not be created.";
+                createStatusTextTwo.Text = "Please enter information in all fields.";
                 createStatusText.FontWeight = FontWeights.Bold;
             }
             else
             {
-                createStatusText.Text = "Creating user...";
-                LoadingBar.IsEnabled = true;
-                LoadingBar.Visibility = Visibility.Visible;
-
-                DbServiceReference.DbServiceClient dbServiceReference = new DbServiceReference.DbServiceClient();
-
-
-                bool checkIfCreated = await dbServiceReference.CreateUserAsync(name, email, password);
-                LoadingBar.IsEnabled = false;
-                LoadingBar.Visibility = Visibility.Collapsed;
-
-                if (checkIfCreated == true)
+                if (password.Length <= 6)
                 {
-                    createStatusText.Text = "Account has successfully been created!";
-                    createStatusTextTwo.Text = "";
-                    createStatusText.FontWeight = FontWeights.Normal;
+                    createStatusText.Text = "The account could not be created.";
+                    createStatusTextTwo.Text = "The length of your password must be longer than 6 characters.";
+                    createStatusText.FontWeight = FontWeights.Bold;
                 }
                 else
                 {
-                    createStatusText.Text = $"The account could not be created.";
-                    createStatusTextTwo.Text = $"An account already exists with the email '{email}'.";
-                    createStatusText.FontWeight = FontWeights.Bold;
+                    if (email.Contains("@"))
+                    {
+                        createStatusText.Text = "Creating user...";
+                        LoadingBar.IsEnabled = true;
+                        LoadingBar.Visibility = Visibility.Visible;
+
+                        DbServiceReference.DbServiceClient dbServiceReference = new DbServiceReference.DbServiceClient();
+
+                        bool checkIfCreated = await dbServiceReference.CreateUserAsync(name, email, password);
+                        LoadingBar.IsEnabled = false;
+                        LoadingBar.Visibility = Visibility.Collapsed;
+
+                        if (checkIfCreated == true)
+                        {
+                            createStatusText.Text = "Account has successfully been created!";
+                            createStatusTextTwo.Text = "";
+                            createStatusText.FontWeight = FontWeights.Normal;
+                        }
+                        else
+                        {
+                            createStatusText.Text = $"The account could not be created.";
+                            createStatusTextTwo.Text = $"An account already exists with the email '{email}'.";
+                            createStatusText.FontWeight = FontWeights.Bold;
+                        }
+                    }
+
+                    else
+                    {
+                        createStatusText.Text = $"The account could not be created.";
+                        createStatusTextTwo.Text = $"'{email}' is not a valid format for an email.";
+                        createStatusText.FontWeight = FontWeights.Bold;
+                    }
                 }
             }
 
--- SalarySystem/SalaryUWP/UpdateUser.xaml.cs
+++ SalarySystem/SalaryUWP/UpdateUser.xaml.cs
@@ -31,14 +31,23 @@ namespace SalaryUWP
 
         private async void UpdateUserClick(object sender, RoutedEventArgs e)
         {
+
             string email = userEmailTb.Text;
             string password = userPasswordTb.Password.ToString();
             string confirmedPassword = userConfirmPasswordTb.Password.ToString();
 
+            if (password.Length <= 6)
+            {
+                updateStatusText.Text = "The password could not be updated.";
+                updateStatusTextTwo.Text = "The length of your password must be longer than 6 characters.";
+                updateStatusTextThree.Text = "";
+                updateStatusText.FontWeight = FontWeights.Bold;
+            }
+
             if (email.Equals("") || password.Equals("") || confirmedPassword.Equals(""))
             {
                 updateStatusText.Text = "The password could not be updated.";
-                updateStatusText.Text = "Please enter all fields correctly.";
+                updateStatusText.Text = "Please enter information in all fields.";
                 updateStatusTextTwo.Text = "";
                 updateStatusTextThree.Text = "";
                 updateStatusText.FontWeight = FontWeights.Bold;
@@ -53,33 +62,42 @@ namespace SalaryUWP
                 }
                 else
                 {
+                    if (password.Length < 6)
+                    {
+                        updateStatusText.Text = "The account could not be created.";
+                        updateStatusTextTwo.Text = "The length of your password must be longer than 6 characters.";
+                        updateStatusText.FontWeight = FontWeights.Bold;
+                    }
+                    else
+                    {
+
+                        updateStatusText.Text = "Updating password...";
+                        LoadingBar.IsEnabled = true;
+                        LoadingBar.Visibility = Visibility.Visible;
+
+                        DbServiceReference.DbServiceClient dbServiceReference = new DbServiceReference.DbServiceClient();
 
 
-                updateStatusText.Text = "Updating password...";
-                LoadingBar.IsEnabled = true;
-                LoadingBar.Visibility = Visibility.Visible;
+                        bool checkIfUpdated = await dbServiceReference.UpdatePasswordAsync(email, confirmedPassword);
+                        LoadingBar.IsEnabled = false;
+                        LoadingBar.Visibility = Visibility.Collapsed;
 
-                DbServiceReference.DbServiceClient dbServiceReference = new DbServiceReference.DbServiceClient();
+                        if (checkIfUpdated == true)
+                        {
+                            updateStatusText.Text = "Password has successfully been updated.";
+                            updateStatusTextTwo.Text = "";
+                            updateStatusTextThree.Text = "";
+                            updateStatusText.FontWeight = FontWeights.Normal;
+                        }
+                        else
+                        {
+                            updateStatusText.Text = $"The password could not be updated.";
+                            updateStatusTextTwo.Text = $"No user with the email '{email}' was found.";
+                            updateStatusTextThree.Text = $"Did you enter the correct email address?";
+                            updateStatusText.FontWeight = FontWeights.Bold;
+                        }
+                    }
 
-
-                bool checkIfUpdated = await dbServiceReference.UpdatePasswordAsync(email, confirmedPassword);
-                LoadingBar.IsEnabled = false;
-                LoadingBar.Visibility = Visibility.Collapsed;
-
-                if (checkIfUpdated == true)
-                {
-                    updateStatusText.Text = "Password has successfully been updated.";
-                    updateStatusTextTwo.Text = "";
-                    updateStatusTextThree.Text = "";
-                    updateStatusText.FontWeight = FontWeights.Normal;
-                }
-                else
-                {
-                    updateStatusText.Text = $"The password could not be updated.";
-                    updateStatusTextTwo.Text = $"No user with the email '{email}' was found.";
-                    updateStatusTextThree.Text = $"Did you enter the correct email address?";
-                    updateStatusText.FontWeight = FontWeights.Bold;
-                }
                 }
             }
 
