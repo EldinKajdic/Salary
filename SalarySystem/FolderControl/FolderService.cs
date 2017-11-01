a28af051706b2f3e28ff7f4b338684c80a12f34d
Eldin Kajdic <eldinkajdic@hotmail.com>
2017-11-01 17:31:55 +0100

    Added installer for Windows Service for FolderControl mover.

---
+++ SalarySystem/FolderControl/FolderService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderControl
+{
+    public partial class FolderService : ServiceBase
+    {
+        public FolderService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Console.WriteLine(CheckFolders());
+
+        }
+
+        private string CheckFolders()
+        {
+            DirectoryInfo listOfFiles = new DirectoryInfo(@"D:\Repos\Salary\SalarySystem\Textfiles\SavedFiles\");
+            string SavedFilesPath = @"D:\Repos\Salary\SalarySystem\Textfiles\SavedFiles\Old-files\";
+            string result = "No files were found.";
+
+            FileInfo[] filer = listOfFiles.GetFiles();
+
+            if (listOfFiles != null)
+            {
+                foreach (FileInfo item in filer)
+                {
+                    item.MoveTo(SavedFilesPath + item.Name);
+                    result = $"The files were moved to {SavedFilesPath}.";
+                }
+            }
+            else
+            {
+                result = "No files were found.";
+            }
+            return result;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
